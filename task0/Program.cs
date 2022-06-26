d4295daa90d52a2b285b50435e7fa87d4df99899
Trofimov Anton <Trofimovanton73@gmail.com>
2022-06-26 12:04:29 +0300

    add task0

---
+++ task0/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число для возведения в квадрат");
+int number = Convert.ToInt32(Console.ReadLine());
+int squareOfNumbers = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {squareOfNumbers}");
