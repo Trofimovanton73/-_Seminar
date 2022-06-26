45ed8d41a7f046297583338721f830b67b6ff48d
Trofimov Anton <Trofimovanton73@gmail.com>
2022-06-26 12:07:56 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Находим квадрат введенного числа
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int squareOfNumbers = numberB * numberB;
+if (squareOfNumbers == numberA)
+{
+    Console.WriteLine($"Число {numberB} является квадратом числа {numberA}");
+}
+else
+{
+    Console.WriteLine($"Число {numberB} не является квадратом числа {numberA}");
+}
