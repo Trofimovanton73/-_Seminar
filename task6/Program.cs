33f1e180817b9cb7f1313fab08970f761c6b4158
Trofimov Anton <Trofimovanton73@gmail.com>
2022-06-26 12:09:55 +0300

    add task6

---
+++ task6/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача №7
+// Напишите программу, которая принимает на вход 
+//  ёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = number % 10;
+Console.WriteLine($"Последнее число: {number2}");
