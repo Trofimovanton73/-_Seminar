843a9a6ffd9cec9e5c620c346266cb75067e6627
Trofimov Anton <Trofimovanton73@gmail.com>
2022-06-26 12:09:26 +0300

    add task5

---
+++ task5/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Напишите программу, которая на вход принимает одно число (N), а на выходе 
+// показывает все целые числа в промежутке от -N до N.
+Console.WriteLine("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+for(int i= -numberN; i<=numberN; i++)
+{
+  Console.Write($"{i},"); 
+}
